2ea581a3eda0649549d794176b77b0bf6cd2b123
VineBast <81682147+VineBast@users.noreply.github.com>
2021-09-28 17:14:58 +0200

    Update LecteurEnvers.java

--- src/Lecteur/LecteurEnvers.java
+++ src/Lecteur/LecteurEnvers.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class LecteurEnvers extends LectureDuFichier implements Lecteur {
+public class LecteurEnvers extends LectureDuFichier {
 	protected BufferedReader br;
 	protected StringBuffer sb;
 	protected String ligne;
